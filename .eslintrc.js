91ba2f8e0656f7726adbd1ca1f350af0f97249ca
Valerii Vasin <valerii.vasin@gmail.com>
2020-10-03 18:30:21 +0200

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-use-before-define': ['error', 'nofunc']
+    '@typescript-eslint/no-use-before-define': ['error', 'nofunc']
   }
 };
