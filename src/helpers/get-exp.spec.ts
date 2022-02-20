48dc49f3c06faa752659cb44990d82678cd64720
Valerii Vasin <valerii.vasin@gmail.com>
2022-02-20 15:22:43 +0100

    Update packages and exp table (#14)
    * chore: bump deps
    
    * feat: update exp table

--- src/helpers/get-exp.spec.ts
+++ src/helpers/get-exp.spec.ts
@@ -8,4 +8,10 @@ describe('Levels difference', () => {
   it('calculates for not precise levels correctly', () => {
     expect(getExp({ from: 53.7, to: 55 })).toBe(76_111_635);
   });
+
+  it('calculates for max level', () => {
+    expect(getExp({ from: 92, to: 93 })).toBe(
+      630_290_119_652_118 - 219_717_789_420_024
+    );
+  });
 });
